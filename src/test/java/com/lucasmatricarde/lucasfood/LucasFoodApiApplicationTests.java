eaf6f8cb9058c07c3c9f00b76b5e6271d4bf2614
Lucas Matricarde <lucas.eduardo@wises.com.br>
2024-11-28 21:27:09 -0300

    Primeiro Commit

---
+++ src/test/java/com/lucasmatricarde/lucasfood/LucasFoodApiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.lucasmatricarde.lucasfood;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class LucasFoodApiApplicationTests {
+
+    @Test
+    void contextLoads () {
+    }
+
+}
